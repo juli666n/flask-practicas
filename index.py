6012ce28fc78605c150d29e312bf14012fc679b6
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2019-11-16 15:38:49 -0500

    add .env

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3307/plantsDatabase'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3307/plantsDatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
